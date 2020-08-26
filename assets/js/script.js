a775fde60c71a55ffcca84b170561d25a6943135
Lina Vallejo <linav92@gmail.com>
2020-08-25 22:18:49 -0400

    call nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 $(function () {
-   
+    $("a").click(function(event) {
+        // Con este if se asegura que this.hash tenga un valor antes de anular el comportamiento predeterminado
+            if (this.hash !== "") {
+        // Previene el comportamiento de click predeterminado
+            event.preventDefault();
+        // Guarda el valor del hash en una variable llamada gato
+            var gato = this.hash;
+        // Usa el método animate para animar el scroll y hacerlo de una forma suave
+        // El número opciona (800) especifica el número de milisegundos que se
+        //demorará en llegar hasta el área
+            $('html, body').animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+        //Ahora guardamos y recargamos el navegador y podemos ver que se genera el scroll suave entre
+        //secciones.
+        // Agrega hash (#) a la URL cuando haya terminado de desplazarse
+        //(comportamiento de click predeterminado)
+            window.location.hash = gato;
+        });
+        } // Fin del if
+        });
 
     $('[data-toggle="tooltip"]').tooltip();
 });
